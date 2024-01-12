37599af186b1797556205b23e1ab6856378e60d6
Kevin <68612569+diyelectromusic@users.noreply.github.com>
2024-01-12 11:00:04 -0800

    Remove line that mods dexed.h.  Now need to do it by hand for now.

--- getsubmod.sh
+++ getsubmod.sh
@@ -1,5 +1,4 @@
 cd ../synth_dexed
 git clone https://codeberg.org/dcoredump/Synth_Dexed.git
-echo "$(echo "#include \"dexed_if_common.h\""; cat Synth_Dexed/src/dexed.h)" > Synth_Dexed/src/dexed.h
 cd ..
 
